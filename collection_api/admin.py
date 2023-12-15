6163633c687fc0030ecf36f0fe5503dbaef573ef
Shri Kalanke <you@example.com>
2023-12-15 12:35:12 +0530

    initial commit...

--- collection_api/admin.py
+++ collection_api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from .models import Collection
 
-from collection_api.models import Collection
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('collection_id', 'collection_date', 'collection_time', 'shopId', 'cashierId', 'total_amount')
+    list_filter = ('collection_date', 'shopId', 'cashierId')
+    search_fields = ('collection_id', 'total_amount', 'cashierId__username')  # Add fields for search
 
-# Register your models here.
-admin.site.register(Collection)
+admin.site.register(Collection, CollectionAdmin)
--- collection_api/views.py
+++ collection_api/views.py
@@ -25,22 +25,43 @@ def ShopModelListView(request):
 @api_view(['POST'])
 def CollectionsAddView(request):
     if request.method == 'POST':
+        required_fields = ['collection_date', 'collection_time', 'shopId', 'cashierId', 'latitude', 'longtitude', 'total_amount', 'fanialize_by']
+        missing_fields = []
 
-        serializer = CollectionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            response_data = {
-                "message_text": "Success",
-                "message_code": 1000,
-                "message_data": serializer.data
-        }
-            return Response(response_data, status=status.HTTP_201_CREATED)
-        
-        return Response({
-                    "message_text": "Failure",
-                    "message_code": 999,
-                    "message_data": [],
+        for field in required_fields:
+            if field not in request.data:
+                missing_fields.append(field)
+
+        if missing_fields:
+            return Response({
+                "message_text": f"Missing fields: {', '.join(missing_fields)}",
+                "message_code": 998,
+                "message_data": [],
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            serializer = CollectionSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                response_data = {
+                    "message_text": "Success",
+                    "message_code": 1000,
+                    "message_data": serializer.data
+                }
+                return Response(response_data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({
+                    "message_text": "Validation error",
+                    "message_code": 997,
+                    "message_data": serializer.errors,
                 }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({
+                "message_text": "An error occurred",
+                "message_code": 996,
+                "message_data": str(e),
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 
 @api_view(['GET'])
